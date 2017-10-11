6c12ef2824a7424dbd88a4594992d5945106b051
Darek <Darek@Tablet-ASUS>
2017-10-11 09:47:46 +0200

    Pierwsze dodanie do Git

---
+++ src/pakiet/Pierwsza.java
@@ -0,0 +1,15 @@
+package pakiet;
+
+public class Pierwsza {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Pierwsza klasa");
+
+		System.out.println("Nowa Linia");		
+
+		System.out.println("Kolejna Linia");
+
+	}
+
+}
