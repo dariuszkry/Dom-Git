716ab7e222d1da2b8d02c4474e17d27686f9047a
Darek <Darek@Tablet-ASUS>
2017-10-11 09:53:23 +0200

    drugie dodanie

---
+++ src/pakiet/KlasaDodana1415.java
@@ -0,0 +1,13 @@
+package pakiet;
+
+public class KlasaDodana1415 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Nowy tekst");
+		
+		System.out.println("Nowy tekst nr.2");
+
+	}
+
+}
